c463c224f531ea1affb11c71accbe1993927af5a
Meth <you@example.com>
2023-06-25 15:57:39 +0200

    add new methods "uscita" and "entrata"

--- src/main/java/redcat/TimbraturaServlet.java
+++ src/main/java/redcat/TimbraturaServlet.java
@@ -3,6 +3,12 @@ package net.javaguides.servlet.tutorial.examples;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import static java.time.format.DateTimeFormatter.*;
+import static java.time.format.FormatStyle.*;
+import org.json.JSONObject;
+import org.json.JSONException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,37 +30,67 @@ public class TimbraturaServlet extends HttpServlet {
 	    catch(ClassNotFoundException e){
 
 	    e.printStackTrace();
+    
 	    }
+
+        JSONObject jsonResp = new JSONObject();
         try{
+        String id = req.getParameter("id");
+        String action = req.getParameter("action");
+        String oggi = LocalDate.now().toString();
+        String orario = LocalTime.now().format(ofLocalizedTime(MEDIUM));
+        resp.setContentType("text/plain");  
+        resp.setCharacterEncoding("UTF-8");
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/controllo_presenze_dipendenti","root","");
         Statement stmt = connection.createStatement();
-         String sql;
-         sql = "SELECT * FROM dipendente";
-         ResultSet rs = stmt.executeQuery(sql);
-         String text = "Ciao Mondo";
-         while (rs.next()){
-            String id = rs.getString("id");
-            String nome = rs.getString("nome");
-            String cognome = rs.getString("cognome");
-            text += id +" "+ nome +" "+ cognome;
+        String checkDuplicateQuery = "SELECT * FROM presenza WHERE id_dipendente = '"+id+"' AND data = '"+oggi+"'";
+        ResultSet rs = stmt.executeQuery(checkDuplicateQuery);
+
+        
+        if (rs.next()){ //se esiste già una riga
+         
+         jsonResp.put("message","LA TUA PRESENZA D'INGRESSO E' GIA STATA REGISTRATA " + rs.getString("ingresso"));
+         jsonResp.put("result","feilure");
+         }else{
+         
+         String sql = "INSERT INTO presenza (data, id_dipendente, ingresso) VALUES ('"+oggi+"', '"+id+"', '"+orario+"')";
+         int numrows = stmt.executeUpdate(sql);
+         
+         if (numrows == 1){ 
+         jsonResp.put("message","PRESENZA REGISTRATA CORRETTAMENTE ALLE ORE: " + orario); 
+         jsonResp.put("result","success");
+
+         }else{
+         jsonResp.put("message","LA PRESENZA NON È STATA INSERITA");
+         jsonResp.put("result","feilure");
+ 
+         }
+         
+        
+        
         }
-        resp.setContentType("text/plain");  
-        resp.setCharacterEncoding("UTF-8"); 
-        resp.getWriter().write(text);
+        resp.getWriter().write(jsonResp.toString());
         rs.close();
         stmt.close();
         connection.close();
-        }catch(SQLException e){
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+            try{
+            jsonResp.put("message"," NON E' STATA POSSIBILE REGISTRARE LA TUA TIMBRATURA ");
+            jsonResp.put("result","feilure");
+            }catch(JSONException ex){
+            ex.printStackTrace();
+        }
+        }
+        catch(JSONException e){
             e.printStackTrace();
         }
-        /*String id = req.getParameter("id");
-        resp.setContentType("text/html");
-        PrintWriter printWriter = resp.getWriter();
-        printWriter.print("<html>");
-        printWriter.print("<body>");
-        printWriter.print("<p> pid :: " + id + "</p>");
-        printWriter.print("</body>");
-        printWriter.print("</html>");
-        printWriter.close();*/
+    }
+     private void gestisciEntrata(){
+
+    }
+     private void gestisciUscita(){
+        
     }
 }
